f071ed432b9dc76ba7b3aa176cffa716b1d63e07
laolang2016 <laolang2016@163.com>
2023-11-19 07:34:03 +0800

    ✨ feat(hello-01): 添加资源

--- hello-01/mainwindow.cpp
+++ hello-01/mainwindow.cpp
@@ -1,8 +1,31 @@
 #include "mainwindow.h"
 
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+
+    resize(800,600);
+
+    QWidget * widget = new QWidget();
+    setCentralWidget(widget);
+
+    QHBoxLayout *layout = new QHBoxLayout();
+
+    QPushButton * btn1 = new QPushButton("按钮一");
+    btn1->setFixedSize(90,27);
+    layout->addWidget(btn1);
+
+    QPushButton * btn2 = new QPushButton();
+    btn2->setFixedSize(90,27);
+    btn2->setIcon(QPixmap(":/image/add.png"));
+    layout->addWidget(btn2);
+
+    centralWidget()->setLayout(layout);
 }
 
 MainWindow::~MainWindow()
